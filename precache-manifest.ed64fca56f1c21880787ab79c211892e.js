5c197fa79a49256800ceab0fb4f8bc6f238adb3d
JayHan <jay.han@madoor.com>
2020-09-22 11:55:57 -0700

    Updates

---
+++ precache-manifest.ed64fca56f1c21880787ab79c211892e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c6fe54827f63fd24251fce060c2da937",
+    "url": "/Resume-React/index.html"
+  },
+  {
+    "revision": "4bea098be6ed8b204d51",
+    "url": "/Resume-React/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "86f8576728f4a437ea5d",
+    "url": "/Resume-React/static/js/2.c0ae24f8.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Resume-React/static/js/2.c0ae24f8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4bea098be6ed8b204d51",
+    "url": "/Resume-React/static/js/main.8bb06d82.chunk.js"
+  },
+  {
+    "revision": "0d609ca7442b2721326d",
+    "url": "/Resume-React/static/js/runtime-main.9c430913.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/Resume-React/static/media/logo.5d5d9eef.svg"
+  }
+]);
